55a2e9cf4318f2e9d6b509923f88c67a2ff7e920
solfigueroa1 <you@example.com>
2023-07-13 21:28:50 -0300

    Arranqué con el registro

--- usuario/urls.py
+++ usuario/urls.py
@@ -7,4 +7,5 @@ app_name= 'usuario'
 urlpatterns = [
     path('login/',views.login, name='login'),
     path('logout/',LogoutView.as_view(template_name='usuario/logout.html'), name='logout'),
+    path('registro/', views.registrarse, name='registrarse'),
 ]
--- usuario/views.py
+++ usuario/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import authenticate, login as django_login
 
 
@@ -23,3 +23,15 @@ def login(request):
     
     formulario = AuthenticationForm()
     return render(request, 'usuario/login.html', {'formulario': formulario})
+
+def registrarse(request):
+    if request.method == "POST":
+        formulario = UserCreationForm(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+            return redirect('usuario:login')
+        else:
+            return render(request, 'usuario/registro.html', {'formulario':formulario})
+    
+    formulario = UserCreationForm()
+    return render(request, 'usuario/registro.html', {'formulario':formulario})
